28c0465cc3b7b092db085a1694cc6d9e4cf9a067
anil <anil.pokhariyal1@gmail.com>
2017-09-23 22:40:23 +0530

    INSERT and UPDATE added on Tomb

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require 'tomb/DB.php';
-$obj = new DB();
-$result = $obj->table('users')->select('*')->get();
+$result = DB::table('users')->select('*')->get();
 echo "<pre>";print_r($result);die;
- ?>
+?>
--- tomb/DB.php
+++ tomb/DB.php
@@ -1,17 +1,19 @@
 <?php
-// error_reporting(0);
-// require_once("server.php");
+error_reporting(0);
 //credit erevolutions (india)
+// For OPEN SOURCE
 class DB{
-  public $table;
+  private $__table='';
   public $connect;
-  public $result = '';
-  public function __construct(){
+  public $query = '';
+  public $where = 1;
+  public function __construct($table){
+    $this->__table = $table;
     require_once("server.php");
     $this->connect = $this->connection($SERVER,$USER,$PASS,$DBNAME);
   }
 
-  public function connection($SERVER='',$USER='',$PASS='',$DBNAME=''){
+  public function connection($SERVER = '', $USER = '', $PASS = '', $DBNAME = '') {
     $connect = mysqli_connect($SERVER,$USER,$PASS,$DBNAME);
     if(!$connect){
       echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -22,30 +24,39 @@ class DB{
     }
   }
 
-  public function table($table=''){
-    $this->table = $table;
-    return $this;
+  public static function table($table){
+     return new DB($table);
   }
 
   public function get(){
     $output = array();
-    while($result = mysqli_fetch_object($this->result)){
+    $response = $this->connect->query($this->query);
+    while($result = mysqli_fetch_object($response)){
       $output[] = $result;
     }
     return (object) $output;
   }
 
   public function select($values){
-    $table = $this->table;
-    $this->result = $this->connect->query("SELECT ".$values." from ".$table);
+    $table = $this->__table;
+    $this->query = "SELECT ".$values." from ".$table;
     return $this;
   }
 
   public function insert($values=array()){
-    $table = $this->table;
-    $query = $connect->query("INSERT INTO ".$table." set ".$values);
+    $table = $this->__table;
+    $query = $this->connect->query("INSERT INTO ".$table." SET ".$values);
     return $query;
   }
 
+  public function update($values=array()){
+    $result = $this->connect->query("UPDATE ".$this->__table." SET ".$values." WHERE ".$where);
+    return $result;
+  }
+
+  public function where($where=array()){
+    $this->where = $array;
+    return $this;
+  }
 }
 ?>
